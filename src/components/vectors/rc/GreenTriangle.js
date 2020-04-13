3f8c095290f04330204024ad354e95ae80fc9903
Billy Watson <billy.watson@thisisabsurd.com>
2020-04-13 18:55:15 +0100

    revert leopard spot vectors, merge in illustrator and recompress

---
+++ src/components/vectors/rc/GreenTriangle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GreenTriangle = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 264">
+      <path fill="#7AB2A1" class="shape" d="M100 264H0V0l100 264z" />
+      <path fill="#7AB2A1" class="circle" d="M55 132c0 2.8-2.2 5-5 5s-5-2.2-5-5 2.2-5 5-5 5 2.2 5 5z" />
+    </svg>
+  );
+};
+
+export default GreenTriangle;
