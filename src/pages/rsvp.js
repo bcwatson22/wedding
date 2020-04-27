84fb7da19798f147842ae83dc2c13ccf80f35be1
Billy Watson <billy.watson@thisisabsurd.com>
2020-04-27 17:14:20 +0100

    tidy up//delete comments after build time queries & splitting rsvp page//comps

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect } from 'react';
 import Helmet from 'react-helmet';
-// import { useQuery } from '@apollo/react-hooks';
-// import gql from 'graphql-tag';
 import { navigate } from 'gatsby';
 
 import LoadingContext from './../context/LoadingContext';
@@ -10,67 +8,18 @@ import RoutingContext from './../context/RoutingContext';
 import Layout from './../components/Layout';
 import Container from './../components/Container';
 import Gatecrasher from './../components/rsvp/Gatecrasher';
-// import Greeting from './../components/rsvp/Greeting';
-// import Form from './../components/rsvp/Form';
-//
+
 import { hasLocalStorage } from './../services/utils';
-//
-// const getGuestInfoQuery = gql`
-//   query guest($id: ID!) {
-//     guest(where: { id: $id }) {
-//       id
-//       personal {
-//         greeting
-//         nicknames
-//       }
-//       contact {
-//         address
-//         email
-//       }
-//       rsvp {
-//         id
-//         responded
-//         date
-//         responses {
-//           id
-//           name
-//           attending
-//           dietary
-//           comments
-//         }
-//       }
-//     }
-//   }
-// `
 
 const RSVP = ({ children, location }) => {
-  // const wrapper = useRef(null);
-  // const [completed, setCompleted] = useState(null);
   const { hideLoading } = useContext(LoadingContext);
   const { hideRouting } = useContext(RoutingContext);
 
-  // const haveLocalStorage = typeof localStorage !== 'undefined';
-  // const urlParams = new URLSearchParams(location.search);
-  // const urlToken = location.search.length ? urlParams.get('guest') : null;
   const userToken = hasLocalStorage() && localStorage.getItem('bb-wedding-guest') ? localStorage.getItem('bb-wedding-guest') : null;
-
-  // const { loading, error, data } = useQuery(getGuestInfoQuery, {
-  //   variables: {
-  //     id: userToken
-  //   },
-  //   skip: !userToken,
-  //   onCompleted(result) {
-  //     hideLoading()
-  //   }
-  // });
 
   useEffect(() => {
 
-
-
     if (userToken) {
-
-      // if (haveLocalStorage) localStorage.setItem('bb-wedding-guest', userToken);
 
       navigate(
         `/rsvp/${userToken}`,
@@ -85,14 +34,6 @@ const RSVP = ({ children, location }) => {
     }
 
   }, [hideLoading, hideRouting, userToken]);
-
-  // const setStatus = (date) => {
-  //
-  //   scrollTo(wrapper.current);
-  //
-  //   setCompleted(date);
-  //
-  // }
 
   return (
     <>
--- src/templates/respond.js
+++ src/templates/respond.js
@@ -1,59 +1,11 @@
-// import React from "react";
-// import { graphql } from "gatsby";
-// import Layout from "../components/Layout";
-//
-// const getGuestInfoQueryBuildtime = graphql`
-//   query getGuest($id: ID!) {
-//     wedding {
-//       guest(where: { id: $id }) {
-//         id
-//         personal {
-//           nicknames
-//           greeting
-//         }
-//         rsvp {
-//           responded
-//           date
-//           responses {
-//             name
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
-//
-// export default ({ data }) => {
-//   const page = data.wedding.guest
-//   return (
-//     <Layout>
-//       <div>
-//         <h1>{JSON.stringify(data)}</h1>
-//         <h2></h2>
-//         <p></p>
-//       </div>
-//     </Layout>
-//   )
-// };
-
-
-
-
-
-
 import React, { useRef, useContext, useEffect } from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
-// import { useQuery } from '@apollo/react-hooks';
-// import gql from 'graphql-tag';
-//
-// import LoadingContext from './../context/LoadingContext';
+
 import RoutingContext from './../context/RoutingContext';
 
 import Layout from './../components/Layout';
 import Container from './../components/Container';
-// import Gatecrasher from './../components/rsvp/Gatecrasher';
-// import Greeting from './../components/rsvp/Greeting';
 import Respond from './../components/rsvp/Respond';
 
 import { hasLocalStorage } from './../services/utils';
@@ -79,80 +31,21 @@ export const getGuestInfoQueryBuildtime = graphql`
   }
 `;
 
-// const getGuestInfoQueryRuntime = gql`
-//   query guest($id: ID!) {
-//     guest(where: { id: $id }) {
-//       id
-//       personal {
-//         greeting
-//         nicknames
-//       }
-//       contact {
-//         address
-//         email
-//       }
-//       rsvp {
-//         id
-//         responded
-//         date
-//         responses {
-//           id
-//           name
-//           attending
-//           dietary
-//           comments
-//         }
-//       }
-//     }
-//   }
-// `;
-
 export default ({ data }) => {
   const wrapper = useRef(null);
-  // const [completed, setCompleted] = useState(null);
-  // const { hideLoading } = useContext(LoadingContext);
   const { hideRouting } = useContext(RoutingContext);
 
   const { guest } = data.wedding;
-  // const haveLocalStorage = typeof localStorage !== 'undefined';
   const userToken = guest.id;
-
-  // const { loading, error, data } = useQuery(getGuestInfoQueryRuntime, {
-  //   variables: {
-  //     id: userToken
-  //   },
-  //   // skip: !userToken,
-  //   onCompleted(result) {
-  //     console.log(result)
-  //     hideLoading()
-  //   }
-  // });
 
   useEffect(() => {
 
     hideRouting();
 
-    // if (userToken) {
+    if (hasLocalStorage()) localStorage.setItem('bb-wedding-guest', userToken);
 
-      if (hasLocalStorage()) localStorage.setItem('bb-wedding-guest', userToken);
-
-    // } else {
-
-      // hideLoading();
-
-    // }
 
   }, [hideRouting, userToken]);
-
-  // const setStatus = (date) => {
-  //
-  //   console.log('setStatus duck')
-  //
-  //   scrollTo(wrapper.current);
-  //
-  //   setCompleted(date);
-  //
-  // }
 
   return (
     <>
