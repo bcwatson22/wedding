aa29ad6b5117eec31e899648e8dd213d70c7d7eb
Billy Watson <billy.watson@thisisabsurd.com>
2020-04-30 14:17:22 +0100

    use React.lazy and Suspense to dynamically import Map component

--- src/components/info/Information.js
+++ src/components/info/Information.js
@@ -1,13 +1,12 @@
 import React, { useContext, lazy, Suspense } from 'react';
 
-import Icon from './../Icon';
-import Map from './Map';
-
 import LoadingContext from './../../context/LoadingContext';
+
+import Icon from './../Icon';
 
 import dovedale from './../../assets/images/dovedale.jpg';
 
-// const Map = lazy(() => import('./Map'));
+const Map = lazy(() => import('./Map'));
 
 const taxiCompanies = [
   {
@@ -54,10 +53,11 @@ const Information = () => {
         <strong property="schema:addressRegion">Derbyshire</strong>
         <strong property="schema:postalCode">DE6 2AD</strong>
       </p>
-      {finishedLoading && <Map />}
-      {/*<Suspense fallback={<h1>Loading...</h1>}>
-        <Map />
-      </Suspense>*/}
+      {finishedLoading &&
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Map />
+        </Suspense>
+      }
       <h2 className="heading--icon"><Icon file="accomodation" folder="info" alt="Accomodation" />Accomodation</h2>
       <p>Craft beer etsy everyday carry cloud bread live-edge health goth ethical roof party microdosing sartorial. Keffiyeh crucifix literally, ennui bushwick gentrify beard snackwave quinoa. Kitsch gochujang synth portland jean shorts chia narwhal bicycle rights 8-bit. Readymade try-hard put a bird on it scenester helvetica sriracha microdosing farm-to-table meggings glossier taxidermy aesthetic. Yr messenger bag PBR&B celiac chicharrones, deep v heirloom fixie marfa microdosing vinyl copper mug sartorial cloud bread trust fund. Migas sustainable vexillologist williamsburg hexagon keytar.</p>
       <h2 className="heading--icon"><Icon file="food" folder="info" alt="Food and drinks" />Food and drinks</h2>
