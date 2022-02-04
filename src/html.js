85cd1888655f7e8294362d13eee271fc41b20937
Billy Watson <billy.watson@thisisabsurd.com>
2022-02-04 18:10:37 +0000

    favicon paths correct

--- src/html.js
+++ src/html.js
@@ -13,22 +13,22 @@ export default function HTML(props) {
         <meta name="theme-color" 
           content="#ffffff" />
         <link rel="icon" 
-          href="public/favicon.ico" />
+          href="/favicon.ico" />
         <link rel="apple-touch-icon" 
           sizes="180x180" 
-          href="public/apple-touch-icon.png" />
+          href="/apple-touch-icon.png" />
         <link rel="icon" 
           type="image/png" 
           sizes="32x32" 
-          href="public/favicon-32x32.png" />
+          href="/favicon-32x32.png" />
         <link rel="icon" 
           type="image/png" 
           sizes="16x16" 
-          href="public/favicon-16x16.png" />
+          href="/favicon-16x16.png" />
         <link rel="manifest" 
-          href="public/manifest.json" />
+          href="/manifest.json" />
         <link rel="mask-icon" 
-          href="public/safari-pinned-tab.svg" 
+          href="/safari-pinned-tab.svg" 
           color="#13161d" />
         {props.headComponents}
       </head>
